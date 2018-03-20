3dc343db6d4ed1d6405f22bd30c7a478d60f342f
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-03-19 22:04:27 -0700

    Hardcode the offset number

--- PLYBinaryExporter.js
+++ PLYBinaryExporter.js
@@ -76,13 +76,13 @@ THREE.PLYBinaryExporter.prototype = {
 		// so we can use a minimal amount of memory / data
 		var indexByteCount = 1;
 
-		if ( vertexCount >= Math.pow( 2, 8 ) ) {
+		if ( vertexCount > 255 ) { // 2^8 bits
 
 			indexByteCount = 2;
 
 		}
 
-		if ( vertexCount >= Math.pow( 2, 16 ) ) {
+		if ( vertexCount > 65535 ) { // 2^16 bits
 
 			indexByteCount = 4;
 
